0a76ca2ccb1c41eedf6a6c7e1079696cabad37ee
aural <eivillamuelas@gmail.com>
2024-05-30 00:46:56 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+function animarPez() {
+    anime({
+        targets: ".pez",
+        keyframes: [
+            {translateY: function () {return anime.random(-300, 300)}},
+            {translateX: function () {return anime.random(-500, 500)}},
+            {translateY: function () {return anime.random(-300, 300)}},
+            {translateX: function () {return anime.random(-500, 500)}},
+            {translateY: function () {return anime.random(-300, 300)}},
+        ],
+        duration: function () {return anime.random(1800, 2800);},
+        easing: "easeOutElastic(1, 1.8)",
+        loop: true,
+    });
+}
+
+animarPez();
+
+n = 1
+
+function animarPulpo() {
+    anime({
+        targets: ".pulpo",
+        translateY: function () { return anime.random(-100, 100);},
+        translateX: function () { return anime.random(-100, 100);},
+        rotate: n*360 + 'deg',
+        duration: 3000,
+        easing: "easeOutElastic(1, 1.8)",
+        loop: false,
+    });
+    n += 1
+}
+
+function animarCoral() {
+    anime({
+        targets: ".coral",
+        skew: ["-12deg","12deg"],
+        duration: function () {return anime.random(1000, 4000);},
+        direction: 'alternate',
+        loop: true,
+        easing: 'easeInOutSine'
+    });
+}
+animarCoral()
+
